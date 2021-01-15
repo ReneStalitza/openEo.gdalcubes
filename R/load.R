084d44a17e26b3c08930d67f6a9ad3f6c44ba51e
ReneStalitza <Renestalitza@web.de>
2021-01-15 16:45:26 +0100

    Update load.R

--- R/load.R
+++ R/load.R
@@ -1,4 +1,4 @@
-#' load demo data
+file.path#' load demo data
 #'
 #' @include Session-Class.R
 #'
@@ -6,27 +6,27 @@
 #' @export
 loadDemoData = function() {
 
-  path = Session$getConfig()$data.path
-  path.demo = paste(path,"L8_demo", sep = "/")
+  demo.path = Session$getConfig()$demo.path
+  file.path = paste(demo.path,"L8_demo", sep = "/")
 
 
-  if (! is.element("L8.db",list.files(path))) {
+  if (! is.element("L8.db",list.files(demo.path))) {
 
-    if (! is.element("L8_demo", list.files(path))) {
+    if (! is.element("L8_demo", list.files(demo.path))) {
 
-      zipfile = paste(path, "L8_demo.zip", sep = "/")
+      zipfile = paste(demo.path, "L8_demo.zip", sep = "/")
       download.file("https://uni-muenster.sciebo.de/s/e5yUZmYGX0bo4u9/download",
                     destfile = zipfile, mode="wb")
-      unzip(zipfile=zipfile, exdir = path.demo)
+      unzip(zipfile=zipfile, exdir = file.path)
       file.remove(zipfile)
     }
 
-    L8.files = list.files(path.demo, pattern = ".tif",
+    L8.files = list.files(file.path, pattern = ".tif",
                           recursive = TRUE, full.names = TRUE)
 
-    create_image_collection(L8.files, format = "L8_SR", out_file = paste(path,"L8.db",sep = "/"))
+    create_image_collection(L8.files, format = "L8_SR", out_file = paste(demo.path,"L8.db",sep = "/"))
   }
-  path.db = paste(path,"L8.db", sep = "/")
+  path.db = paste(demo.path,"L8.db", sep = "/")
   ic =image_collection(path.db)
 
   L8 = Collection$new(id = "L8", title = "Landsat8", description = "Landsat")
